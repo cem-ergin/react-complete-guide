cb8915ce1d6427ea4aca9372c600de76b91837c1
Cem Ergin <cem-ergin@outlook.com>
2022-12-25 18:34:53 +0000

    Section 18 is completed - Diving into Redux

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
-import { useState } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import Counter from "./components/Counter";
+import React from "react";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
 function App() {
-  const dummy_expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-  const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
-    setExpenses((prevExpenses) => {
-      console.log(`expense: ${expense}`)
-      return [expense, ...prevExpenses];
-    });
-  };
-
-  const [expenses, setExpenses] = useState(dummy_expenses);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   return (
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses expenses={expenses} />
-    </div>
+    <>
+      <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      <Counter />
+    </>
   );
 }
 
